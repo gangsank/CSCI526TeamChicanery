5f5522cafbbb79969b5c9350a99c2556534ed726
Lorne Chen <laurenchen0631@gmail.com>
2023-03-05 00:15:41 -0800

    menu elements

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,11 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
